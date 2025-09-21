ed526a2a35cc7ed32ec1e34a75dee4e5c603ef6a
Allan Wu <allan.wu@outlook.com.au>
2025-09-21 21:21:01 +0800

    Update ultrasonic.h

--- time_data_plot/ultrasonic.h
+++ time_data_plot/ultrasonic.h
@@ -1,3 +1,6 @@
+// Ultrasonic header file
+// Allan Wu (23810308)
+// Updated 21 September 2025
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
@@ -20,8 +23,10 @@ extern int ultrasonicTimeoutMicroseconds;
 // (currently not used)
 extern int pollDelayMicroseconds;
 
-void setupUltrasonicSensor(int triggerPin = 1, int echoPin = 2, int timeoutMicroseconds = 11500, bool serialEnable = false);
+// Must run this before polling the ultrasonic sensor, can use different pins and set a longer timeout if needed
+void setupUltrasonicSensor(int triggerPin = 1, int echoPin = 2, int timeoutMicroseconds = 15000, bool serialEnable = false);
 
+// Currently this returns the elapsed time in microseconds and updates global variables (implementation can be changed)
 unsigned long pollUltrasonicSensor();
 
 #endif
