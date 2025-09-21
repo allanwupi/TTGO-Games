9d34c9d049866b7fcffa43b917296f2f110b769a
Allan Wu <allan.wu@outlook.com.au>
2025-09-21 23:07:18 +0800

    Toggle gridlines

--- time_data_plot/main.cpp
+++ time_data_plot/main.cpp
@@ -127,12 +127,12 @@ void loop() {
     currLeft = !digitalRead(LEFT_BUTTON);
     currRight = !digitalRead(RIGHT_BUTTON);
     if (prevLeft && !currLeft) { // Press left button to turn off scrolling and refresh grid
-      enableGridlines = true;
+      enableGridlines = !enableGridlines;
       drawGrid();
-      //enableScrolling = false;
+      enableScrolling = false;
     } else if (prevRight && !currRight) { // Press right button to enable scrolling
-      enableGridlines = false;
-      //enableScrolling = true;
+      enableGridlines = !enableGridlines;
+      enableScrolling = true;
     }
     if (sampleIndex > 0 && writeIndex == 0) { // Refresh grid when looping back to start of buffer
       if (autoRanging) { // Auto-ranging: take double the average or 1.5x the running maximum 
