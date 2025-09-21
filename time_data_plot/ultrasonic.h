67c896926af07b7deffcca5d9278224e3e09eb4e
Allan Wu <allan.wu@outlook.com.au>
2025-09-21 21:16:31 +0800

    Update ultrasonic.h

--- time_data_plot/ultrasonic.h
+++ time_data_plot/ultrasonic.h
@@ -3,13 +3,26 @@
 
 // Ultrasonic header file
 
-// Global variable that can be referenced elsewhere, note that a negative value indicates no enemy found
+extern int ULTRASONIC_TRIGGER;
+extern int ULTRASONIC_ECHO;
+
+// Global data variables that can be used in other files
+// Note that sentinel value -1 means "NO ENEMY FOUND"
+extern double ultrasonicDistanceCm;
 extern int ultrasonicDistanceNearestCm;
 
 // Set ultrasonicSerialEnable to 1 to enable printout to the serial monitor
-extern int ultrasonicSerialEnable;
+extern bool ultrasonicSerialEnable;
 
-void setupUltrasonicSensor(bool outputEnable = false);
+// Set the maximum time we will wait for the echo pulse: this determines what is "OUT OF RANGE" for the sensor!
+// From my testing, 15000 us timeout limits the distane range to ~200 cm
+extern int ultrasonicTimeoutMicroseconds;
+
+// Time to wait before running the loop again, note that maximum polling rate is 50 Hz => 20,000 us delay between reads
+// (currently not used)
+extern int pollDelayMicroseconds;
+
+void setupUltrasonicSensor(int triggerPin = 1, int echoPin = 2, int timeoutMicroseconds = 11500, bool serialEnable = false);
 
 unsigned long pollUltrasonicSensor();
 
